bf7b66da79537ef8b988b4840b9d3762077ee189
Alan Licerio <alanlicerio@Alans-MacBook-Pro-2.local>
2020-06-08 14:27:55 -0600

    Form 2 tables logic 90% done. Need to fix bug on add row summation.

--- public/docs/js/form2FrontEndLogic.js
+++ public/docs/js/form2FrontEndLogic.js
@@ -9,34 +9,26 @@ function funding_vehicles_table() {
         let h = "";
         h = parseInt($(this).val());
 
-        if($(this).attr("id")== "federal_vehicles"){
-            console.log($(this).attr("id"));
+        if($(this).attr("id") == "federal_vehicles"){
             if(h >= 0){
                 federal_sum+=h;
                 total_sum+=h;
             }
         }
-
         else if($(this).attr("id")== "local_vehicles"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 local_sum+=h;
                 total_sum+=h;
-
             }
         }
-
         else if($(this).attr("id")== "local_beyond_vehicles"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 local_beyond_sum+=h;
                 total_sum+=h;
 
             }
         }
-
         else if($(this).attr("id")== "tdc_vehicles"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 tdc_sum+=h;
             }
@@ -48,29 +40,32 @@ function funding_vehicles_table() {
     document.getElementById("tdc_vehicles_total").value = tdc_sum;
     document.getElementById("total_vehicles_total").value = total_sum;
     document.getElementById("yoe_check_vehicles").value = total_sum;
+
+    rowSumMaster_1()
 }
 
-function rowSumMaster() {
+function rowSumMaster_1() {
     var table = document.getElementById("fundingVehiclesTable")
     for(let i = 0; i < table.rows.length; i++) {
         var row = table.rows[i];
-        rowSum(row.id,i);
+        rowSum_1(row.id,i);
     }
 }
 
-function rowSum(idName, index) {
+function rowSum_1(idName, index) {
     index = parseInt(index);
     let toSearch = "#" + idName + ' :input';
     let rowTot = 0;
     let totId = "";
     var inputValues = $(toSearch).map(function() {
         let h = $(this).val();
-        if(parseInt($(this).val())>0 && $(this).attr("name") != "funding_total_vehicles" && $(this).attr("name") != "funding_category_vehicles") {
+        if(parseInt($(this).val())>0 && $(this).attr("name") != "funding_total_vehicles" && $(this).attr("name") != "funding_category_vehicles" 
+        && $(this).attr("name") != "funding_tdc_vehicles") {
             h = parseInt($(this).val());
             rowTot+=h;
         }
     })
-    if (index == 0) totId = "fvt1_tot0";
+    if (index == 0) totId = 'fvt1_tot0';
     else {
         index++;
         totId = "fvt1_tot" + index;
@@ -78,7 +73,7 @@ function rowSum(idName, index) {
     $("#" + totId).attr("value",rowTot);
 }
 
-function addRow() {
+function addRow_1() {
     var table = document.getElementById("fundingVehiclesTable");
     var row = table.insertRow(table.rows.length);
 
@@ -96,10 +91,10 @@ function addRow() {
     cell2.innerHTML = '<input onchange="funding_vehicles_table()" id="federal_vehicles" type="number" name="funding_federal_vehicles[]" class="form-control">'
     cell3.innerHTML = '<input onchange="funding_vehicles_table()" id = "local_vehicles" type="number" name="funding_local_vehicles[]" class="form-control">'
     cell4.innerHTML = '<input onchange="funding_vehicles_table()" id = "local_beyond_vehicles" type="number" name="funding_local_beyond_vehicles[]" class="form-control">'
-    cell5.innerHTML = '<input type="number" name="funding_total_vehicles" id="fvt1_tot0" class="form-control" readonly>'
+    cell5.innerHTML = '<input type="number" name="funding_total_vehicles" class="form-control" readonly>'
     cell6.innerHTML = '<input onchange="funding_vehicles_table()" id = "tdc_vehicles" type="number" name="funding_tdc_vehicles[]" class="form-control">'
 
-    let inputId = $(table.rows[table.rows.length -1].cells[4].find("input")[0]);
+    let inputId = $(table.rows[table.rows.length -1].cells[4]).find("input")[0];
     inputId.setAttribute('id',newIdTotal);
 }
 
@@ -115,49 +110,90 @@ function funding_bus_table() {
         h = parseInt($(this).val());
 
         if($(this).attr("id")== "federal_bus"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 federal_sum+=h;
+                total_sum+=h;
             }
         }
-
         else if($(this).attr("id")== "local_bus"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 local_sum+=h;
+                total_sum+=h;
             }
         }
-
         else if($(this).attr("id")== "local_beyond_bus"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 local_beyond_sum+=h;
+                total_sum+=h;
             }
         }
-
         else if($(this).attr("id")== "tdc_bus"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 tdc_sum+=h;
             }
         }
-        else{
-            total_sum += federal_sum+local_sum+local_beyond_sum;
-        }
     })
-    console.log(federal_sum);
-    console.log(local_sum);
-    console.log(local_beyond_sum);
-    console.log(tdc_sum);
-    console.log(total_sum);
-
     document.getElementById("federal_bus_total").value = federal_sum;
     document.getElementById("local_bus_total").value = local_sum;
     document.getElementById("local_beyond_bus_total").value = local_beyond_sum;
     document.getElementById("tdc_bus_total").value = tdc_sum;
-    document.getElementById("total_bus").value = total_sum;
     document.getElementById("total_bus_total").value = total_sum;
     document.getElementById("yoe_check_bus").value = total_sum;
+
+    rowSumMaster_2()
+}
+
+function rowSumMaster_2() {
+    var table = document.getElementById("fundingBusTable")
+    for(let i = 0; i < table.rows.length; i++) {
+        var row = table.rows[i];
+        rowSum_2(row.id,i);
+    }
+}
+
+function rowSum_2(idName, index) {
+    index = parseInt(index);
+    let toSearch = "#" + idName + ' :input';
+    let rowTot = 0;
+    let totId = "";
+    var inputValues = $(toSearch).map(function() {
+        let h = $(this).val();
+        if(parseInt($(this).val())>0 && $(this).attr("name") != "funding_total_bus" && $(this).attr("name") != "funding_category_bus" 
+        && $(this).attr("name") != "funding_tdc_bus") {
+            h = parseInt($(this).val());
+            rowTot+=h;
+        }
+    })
+    if (index == 0) totId = 'fbt1_tot0';
+    else {
+        index++;
+        totId = "fbt1_tot" + index;
+    }
+    $("#" + totId).attr("value",rowTot);
+}
+
+function addRow_2() {
+    var table = document.getElementById("fundingBusTable");
+    var row = table.insertRow(table.rows.length);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+
+    let newIdTotal = "fbt1_tot" + table.rows.length;
+    row.setAttribute('id', 'fbtrow' + table.rows.length);
+
+    cell1.innerHTML = '<input type="text" name="funding_category_bus" class="form-control">'
+    cell2.innerHTML = '<input onchange="funding_bus_table()" id = "federal_bus" type="number" name="funding_federal_bus[]" class="form-control">'
+    cell3.innerHTML = '<input onchange="funding_bus_table()" id = "local_bus" type="number" name="funding_local_bus[]" class="form-control">'
+    cell4.innerHTML = '<input onchange="funding_bus_table()" id = "local_beyond_bus" type="number" name="funding_local_beyond_bus[]" class="form-control">'
+    cell5.innerHTML = '<input type="number" name="funding_total_bus" class="form-control" readonly>'
+    cell6.innerHTML = '<input onchange="funding_bus_table()" id = "tdc_bus" type="number" name="funding_tdc_bus[]" class="form-control">'
+    let inputId = $(table.rows[table.rows.length -1].cells[4]).find("input")[0];
+    inputId.setAttribute('id',newIdTotal);
 }
 
 function funding_operations_table() {
@@ -171,39 +207,80 @@ function funding_operations_table() {
         h = parseInt($(this).val());
 
         if($(this).attr("id")== "federal_operations"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 federal_sum+=h;
+                total_sum+=h
             }
         }
-
         else if($(this).attr("id")== "local_operations"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 local_sum+=h;
+                total_sum+=h
             }
         }
-
         else if($(this).attr("id")== "local_beyond_operations"){
-            console.log($(this).attr("id"));
             if(h >= 0){
                 local_beyond_sum+=h;
+                total_sum+=h
             }
         }
-
-        else{
-            total_sum += federal_sum+local_sum+local_beyond_sum;
-        }
     })
-    console.log(federal_sum);
-    console.log(local_sum);
-    console.log(local_beyond_sum);
-    console.log(total_sum);
 
     document.getElementById("federal_operations_total").value = federal_sum;
     document.getElementById("local_operations_total").value = local_sum;
     document.getElementById("local_beyond_operations_total").value = local_beyond_sum;
-    document.getElementById("total_operations").value = total_sum;
     document.getElementById("total_operations_total").value = total_sum;
     document.getElementById("yoe_check_operations").value = total_sum;
+
+    rowSumMaster_3()
+}
+
+function rowSumMaster_3() {
+    var table = document.getElementById("fundingOperationsTable")
+    for(let i = 0; i < table.rows.length; i++) {
+        var row = table.rows[i];
+        rowSum_3(row.id,i);
+    }
+}
+
+function rowSum_3(idName, index) {
+    index = parseInt(index);
+    let toSearch = "#" + idName + ' :input';
+    let rowTot = 0;
+    let totId = "";
+    var inputValues = $(toSearch).map(function() {
+        let h = $(this).val();
+        if(parseInt($(this).val())>0 && $(this).attr("name") != "funding_total_operations" && $(this).attr("name") != "funding_category_operations") {
+            h = parseInt($(this).val());
+            rowTot+=h;
+        }
+    })
+    if (index == 0) totId = 'fot1_tot0';
+    else {
+        index++;
+        totId = "fot1_tot" + index;
+    }
+    $("#" + totId).attr("value",rowTot);
+}
+
+function addRow_3() {
+    var table = document.getElementById("fundingOperationsTable");
+    var row = table.insertRow(table.rows.length);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    let newIdTotal = "fot1_tot" + table.rows.length;
+    row.setAttribute('id', 'fotrow' + table.rows.length);
+
+    cell1.innerHTML = '<input type="text" name="funding_category_operations" class="form-control">'
+    cell2.innerHTML = '<input onchange="funding_operations_table()" id = "federal_operations" type="number" name="funding_federal_operations[]" class="form-control">'
+    cell3.innerHTML = '<input onchange="funding_operations_table()" id = "local_operations" type="number" name="funding_local_operations[]" class="form-control">'
+    cell4.innerHTML = '<input onchange="funding_operations_table()" id = "local_beyond_operations" type="number" name="funding_local_beyond_operations[]" class="form-control">'
+    cell5.innerHTML = '<input type="number" name="funding_total_operations" class="form-control" readonly>'
+    let inputId = $(table.rows[table.rows.length -1].cells[4]).find("input")[0];
+    inputId.setAttribute('id',newIdTotal);
 }
--- resources/views/projects/create2.blade.php
+++ resources/views/projects/create2.blade.php
@@ -439,7 +439,7 @@
                                         <input type="number" name="funding_tdc_vehicles_total" id = "tdc_vehicles_total" class="form-control" readonly>
                                     </div>
                                 </div>
-                                <a onclick = "addRow()" class="btn btn-primary" role="button">Add Funding</a>
+                                <a onclick = "addRow_1()" class="btn btn-primary" role="button">Add Funding</a>
                                 <a class="btn btn-primary" role="button">Remove Funding</a>
                             </div>
                         </div>
@@ -477,19 +477,18 @@
                                     <div class="form-row mb-1">
                                         <table id="fundingBusTable">
                                             <tr id="fbtrow" class="fbt1">   
-                                                    <td><input type="text" name="funding_category_bus" class="form-control">
-                                                    </td>                                            
-                                                    <td><input onchange="funding_bus_table()" id = "federal_bus" type="number" name="funding_federal_bus" class="form-control">
-                                                    </td>                                               
-                                                    <td><input onchange="funding_bus_table()" id = "local_bus" type="number" name="funding_local_bus" class="form-control">
-                                                    </td>                                                
-                                                    <td><input onchange="funding_bus_table()" id = "local_beyond_bus" type="number" name="funding_local_beyond_bus" class="form-control">
-                                                    </td>                                                
-                                                    <td><input type="number" id="total_bus" name="funding_total_bus" class="form-control" readonly>
-                                                    </td>
-                                                    <td><input onchange="funding_bus_table()" id = "tdc_bus" type="number" name="funding_tdc_bus" class="form-control">
-                                                    </td>
-                                                </div>
+                                                <td><input type="text" name="funding_category_bus" class="form-control">
+                                                </td>                                            
+                                                <td><input onchange="funding_bus_table()" id = "federal_bus" type="number" name="funding_federal_bus" class="form-control">
+                                                </td>                                               
+                                                <td><input onchange="funding_bus_table()" id = "local_bus" type="number" name="funding_local_bus" class="form-control">
+                                                </td>                                                
+                                                <td><input onchange="funding_bus_table()" id = "local_beyond_bus" type="number" name="funding_local_beyond_bus" class="form-control">
+                                                </td>                                                
+                                                <td><input type="number" id="fbt1_tot0" name="funding_total_bus" class="form-control" readonly>
+                                                </td>
+                                                <td><input onchange="funding_bus_table()" id = "tdc_bus" type="number" name="funding_tdc_bus" class="form-control">
+                                                </td>
                                             </tr>
                                         </table>
                                     </div>
@@ -514,7 +513,7 @@
                                         <input type="number" id="tdc_bus_total" name="funding_tdc_bus_total" class="form-control" readonly>
                                     </div>
                                 </div>
-                                <a class="btn btn-primary" role="button">Add Funding</a>
+                                <a onclick = "addRow_2()" class="btn btn-primary" role="button">Add Funding</a>
                                 <a class="btn btn-primary" role="button">Remove Funding</a>
                             </div>
                         </div>
@@ -529,10 +528,10 @@
                                     <div class="col-sm-2">
                                         Operations
                                     </div>
-                                    <div class="col-sm-3">
+                                    <div class="col-sm-2">
                                         Federal Share Usually 50%
                                     </div>
-                                    <div class="col-sm-3">
+                                    <div class="col-sm-2">
                                         Local Share Usually 50%
                                     </div>
                                     <div class="col-sm-2">
@@ -547,41 +546,38 @@
                             <div class="card-body">
                                 <div id="funding_operations">
                                     <div class="form-row mb-1">
-                                        <div class="col-sm-2">
-                                            <input type="text" name="funding_category_operations" class="form-control">
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <input onchange="funding_operations_table()" id = "federal_operations" type="number" name="funding_federal_operations" class="form-control">
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <input onchange="funding_operations_table()" id = "local_operations" type="number" name="funding_local_operations" class="form-control">
-                                        </div>
-                                        <div class="col-sm-2">
-                                            <input onchange="funding_operations_table()" id = "local_beyond_operations" type="number" name="funding_local_beyond_operations" class="form-control">
-                                        </div>
-                                        <div class="col-sm-2">
-                                            <input type="number" id="total_operations" name="funding_total_operations" class="form-control" readonly>
-                                        </div>
+                                        <table id="fundingOperationsTable">
+                                            <tr id="fotrow" class="fot1"> 
+                                                <td><input type="text" name="funding_category_operations" class="form-control">
+                                                </td>
+                                                <td><input onchange="funding_operations_table()" id = "federal_operations" type="number" name="funding_federal_operations" class="form-control">
+                                                </td>
+                                                <td><input onchange="funding_operations_table()" id = "local_operations" type="number" name="funding_local_operations" class="form-control">
+                                                </td>
+                                                <td><input onchange="funding_operations_table()" id = "local_beyond_operations" type="number" name="funding_local_beyond_operations" class="form-control">
+                                                </td>
+                                                <td><input type="number" id="fot1_tot0" name="funding_total_operations" class="form-control" readonly>
+                                                </td>
+                                            </tr>
+                                        </table>
                                     </div>
                                 </div>
                                 <div class="form-row mb-1">
-                                    <div class="col-sm-2">
-                                        Total Funding By Share
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <input type="number" id="federal_operations_total" name="funding_federal_operations_total" class="form-control" readonly>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <input type="number" id="local_operations_total" name="funding_local_operations_total" class="form-control" readonly>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <input type="number" id="local_beyond_operations_total" name="funding_local_beyond_operations_total" class="form-control" readonly>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <input type="number" id="total_operations_total" name="funding_total_operations_total" class="form-control" readonly>
-                                    </div>
+                                    <table>
+                                        <td>Total Funding By Share&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        </td>              
+                                        <td><input type="number" id="federal_operations_total" name="funding_federal_operations_total" class="form-control" readonly>
+                                        </td>
+                                        <td><input type="number" id="local_operations_total" name="funding_local_operations_total" class="form-control" readonly>
+                                        </td>
+                                        <td><input type="number" id="local_beyond_operations_total" name="funding_local_beyond_operations_total" class="form-control" readonly>
+                                        </td>
+                                        <td><input type="number" id="total_operations_total" name="funding_total_operations_total" class="form-control" readonly>
+                                        </td>
+                                    </table>
                                 </div>
-                                <a class="btn btn-primary" role="button">Add Funding</a>
+                                <a onclick = "addRow_3()" class="btn btn-primary" role="button">Add Funding</a>
                                 <a class="btn btn-primary" role="button">Remove Funding</a>
                             </div>
                         </div>
