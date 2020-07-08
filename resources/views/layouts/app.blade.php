2277b7fbdda963b6ec32c7e7d712c24f7c4a372a
Brian <bacardiel@miners.utep.edu>
2020-07-08 14:37:26 -0600

    added comment and bold

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,8 @@
     <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://cdn.rawgit.com/bjornharrtell/jsts/gh-pages/1.4.0/jsts.min.js"></script>
+    
+    <!--Allows icons --->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Edit User</div>
+                <div class="card-header"><b>Edit User</b></div>
                 <div class="card-body">
                     <form action="{{ route('users.update', $user->id) }}" method="POST">
                         @csrf
